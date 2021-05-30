b1bf69b3e7aab4c51aac5f3558841f5a5fa210f1
Abilash-star <85052410+Abilash-star@users.noreply.github.com>
2021-05-30 20:02:15 +0530

    Update Sort_Names_Json.py
    Sorted the names

--- Sort_Names_Json.py
+++ Sort_Names_Json.py
@@ -2,12 +2,14 @@
     
 import urllib.request as url_data
 import json
+import pprint
 
 with url_data.urlopen("http://api.open-notify.org/astros.json") as resp_obj:
     if resp_obj.getcode() == 200:
-        src_data = resp_obj.read()
+        src_data = resp_obj.read().decode("utf-8")
         _json = json.loads(src_data)
-        for i in _json["people"]:
-            print(i["name"])
+        obj_convert = [i["name"] for i in _json["people"]]
+        data = sorted(sort_data for sort_data in obj_convert)
+        print(data)
     else:
-        print("Http Error:", resp_obj.getcode())    
+        print("Http Error:", resp_obj.getcode())   
